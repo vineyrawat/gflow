a5a63f4947af01162e83afc7cd936c2f88e98e21
Vinay Rawat <vineyrawat@yahoo.com>
2023-06-27 12:46:54 +0530

    Added Profile and Account in the session object

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,37 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { use } from "react";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
+    // GoogleProvider({
+    // clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    // clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    // }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async session({ session, token }: any) {
+      if (session.user) {
+        session.user.profile = token.profile;
+        session.user.account = token.account;
+      }
+
+      return Promise.resolve(session);
+    },
+    async jwt({ account, token, profile }) {
+      if (account) {
+        token.account = account;
+      }
+
+      if (profile) {
+        token.profile = profile;
+      }
+
+      return Promise.resolve(token);
+    },
+  },
 });
